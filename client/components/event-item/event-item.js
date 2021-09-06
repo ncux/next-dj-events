a8dee8998cab7c73cdd809e0fd1033fa16707845
ncux <malabatalent@gmail.com>
2021-09-06 11:24:33 +0800

    P3 - data fetching

---
+++ client/components/event-item/event-item.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./event-item.module.css";
+
+export default function EventItem({ evt }) {
+
+    const { date, image, name, slug, time } = evt;
+
+    return (
+        <div className={classes.event}>
+            <div className={classes.image}>
+                <Image src={ image ? image : '/images/event-default.png' } alt="event image" width={170} height={100} />
+            </div>
+            <div className={classes.info}>
+                <span>{ date } at { time }</span>
+                <h3>{ name ? name : '' }</h3>
+            </div>
+
+            <Link href={`/events/${slug}`}>
+                <a className="btn">View Event</a>
+            </Link>
+
+        </div>
+    );
+}
--- client/pages/events/index.js
+++ client/pages/events/index.js
@@ -1,11 +1,20 @@
-
+import EventItem from "../../components/event-item/event-item";
 import Layout from "../../components/layout/layout";
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
 
     return (
         <Layout title="DJ Events - All Events">
-            <h2>Events Page</h2>
+            <h2>Upcoming Events</h2>
+            { events.length === 0 && (<h2>No events</h2>) }
+
+            {
+                events.map(evt => {
+                    return (
+                        <EventItem key={evt.id} evt={ evt } />
+                    )
+                })
+            }
         </Layout>
     );
 }
